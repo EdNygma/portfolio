01dec87071b5f7fb0ce908408e952a8d9bf00d15
kevin <msrmormink@gmail.com>
2021-05-03 20:40:11 +0200

    Design the homepage with links, add Articles caffold and set devise_V_1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :articles
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root to: 'pages#home'  
-  root to: 'pages#home'
 
+  root to: 'pages#home'
+  resources   :posts
 end
